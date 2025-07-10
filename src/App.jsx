22853839c41de12bed9efc659cad6ad81eccb926
CHAMRTHI VENKATESH <160413645+venkatesh767408@users.noreply.github.com>
2025-07-10 13:58:21 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,3 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Navbar from './components/Navbar';
-// import '@fortawesome/fontawesome-free/css/all.min.css';
-
-// import Footer from './components/Footer';
-
-// import EmployersList from './pages/EmployersList';
-// import EmployersSingle from './pages/EmployersSingle';
-// import EmployersDashboard from './pages/EmployersDashboard';
-
-// function App() {
-//   return (
-//     <Router>
-//       <Navbar />
-//       <Routes>
-//         <Route path="/employers/list" element={<EmployersList />} />
-//         <Route path="/employers/single" element={<EmployersSingle />} />
-//         <Route path="/employers/dashboard" element={<EmployersDashboard />} />
-//       </Routes>
-//       <Footer />
-//     </Router>
-//   );
-// }
-
-// export default App;
-// App.jsx
-// App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
